6c29d4031c8522969deeba0e0d4a0d6d852c6d1f
unknown <shriyaborkar71@gmail.com>
2024-08-31 01:09:57 +0530

    Completed score keeper App

--- src/App.js
+++ src/App.js
@@ -1,88 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import "./App.css"
-import ImagePizza from "./pizza-img.jpg"
-import ImageMinus from "./minus.png"
-import ImagePlus from "./add.png"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import GirlsImg from "./girls-image.jpg";
+import BoysImg from "./boys-image.jpg";
+import ImageMinus from "./minus.png";
+import ImagePlus from "./add.png";
 
-function App() {
+const ScoreCard = ({ teamName, teamDescription, teamImage, changeScore }) => {
+  const [score, setScore] = useState(1);
 
-  const PRODUCT_PRICE = 350
-
-  const [quantity, setQuantity] = useState(1);
-  const [totalBill, setTotalBill] = useState(PRODUCT_PRICE)
-
-  const updateQuantity = (type) => {
+  const updateScore = (type) => {
     if (type === "plus") {
-      setQuantity(quantity + 1)
+      setScore(score + 1);
+    } else if (type === "minus") {
+      setScore(score - 1);
     }
-    else if (type === "minus" && quantity > 1) {
-      setQuantity(quantity - 1)
-    }
-  }
+  };
 
-  useEffect (()=>{
-
-    const total = PRODUCT_PRICE * quantity 
-
-    if(quantity < 5){
-      setTotalBill( total )
-    }
-    else if(quantity>=5 && quantity <= 10){
-      setTotalBill( total - 40 )
-    }
-    else{
-      setTotalBill( total - 100)
-    }
-  }, [quantity])
+  useEffect(() => {
+    changeScore(score);
+  }, [score]);
 
   return (
-    <div className='body-container'>
-      <h1 className='heading'>Product Increament decreament Card</h1>
+    <div className="body-container">
       <div className="card-container">
-        <img src={ImagePizza} alt="Pizza" className="card-image" />
-        <h2 className="card-title">Pizza</h2>
-        <p className="card-description">
-          Pizza is an Italian dish typically consisting of a flat base of
-          leavened wheat-based dough topped with tomato, cheese, and other
-          ingredients.
-        </p>
+        <img src={teamImage} alt="Pizza" className="card-image" />
+        <h2 className="card-title">{teamName}</h2>
+        <p className="card-description">{teamDescription}</p>
 
-        <div className='card-product-price'>
-          <span className='card-price'>Price: ₹{PRODUCT_PRICE} </span>
-          <span className='card-price-1'>Toatl: ₹{totalBill}</span>
-        </div>
-
-        <p className='card-error'>
-          {quantity > 10 ? "For bulk orders, delivery time may vary": null}
-        </p>
-
-        <div className='card-quantity-conatainer'>
+        <div className="card-score-conatainer">
           <img
             src={ImageMinus}
-            alt='Minus'
-            className='card-quantity-symbol'
+            alt="Minus"
+            className="card-score-symbol"
             onClick={() => {
               //setQuantity(quantity-1)
-              updateQuantity("minus")
+              updateScore("minus");
             }}
           />
-          <span className='card-quantity'>{quantity}</span>
+          <span className="card-score">{score}</span>
 
           <img
             src={ImagePlus}
-            alt='Plus'
-            className='card-quantity-symbol'
+            alt="Plus"
+            className="card-score-symbol"
             onClick={() => {
               //setQuantity(quantity+1)
-              updateQuantity("plus")
+              updateScore("plus");
             }}
           />
         </div>
-
-        <button className="card-btn">Buy Now</button>
       </div>
     </div>
-  )
+  );
+};
+
+const WinnerCard = ({ score1, score2, setShowWinner }) => {
+  let winner = "";
+
+  if (score1 === score2) {
+    winner = " Match is Draw"
+  }
+  else if(score1 > score1){
+    winner = "Team 1 Won"
+  }
+  else{
+    winner = "Team 2 Won"
+  }
+
+  return (<div className="winner-card">
+    <div className="winner-text">
+    {winner}
+    <button 
+    type="button" 
+    className="close-button" 
+    onClick={()=>setShowWinner(false)}>Close</button>
+    </div>
+    
+    </div>);
+};
+
+function App() {
+  const [team1Score, setTeam1Score] = useState(0);
+  const [team2Score, setTeam2Score] = useState(0);
+  const [showWinner, setShowWinner] = useState(false);
+
+  return (
+    <div className="scorecard-main-conatiner">
+      <h1 className="heading"> 🎯🏑⚽Score Keeper⚽🏑🎯</h1>
+
+      <div className="scorecard-container">
+        <ScoreCard
+          teamName={"Nagpur Queens"}
+          teamDescription={"We are the Queens of Nagpur"}
+          teamImage={GirlsImg}
+          changeScore={setTeam1Score}
+        />
+        <ScoreCard
+          teamName={"Nagpur Kings"}
+          teamDescription={"We are the Kings of Nagpur"}
+          teamImage={BoysImg}
+          changeScore={setTeam2Score}
+        />
+      </div>
+
+      {showWinner ? (
+        <WinnerCard score1={team1Score} score2={team2Score} setShowWinner={setShowWinner}/>
+      ) : null}
+      <button type="button" className="card-btn" onClick={()=>setShowWinner(true)}>
+        Who won?
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div style={{
-  backgroundColor: "#e6f2ff",
-  width: "100%",
-  height: "100vh",
-
-}}>
+root.render(<div >
   <App />
 </div>);
 
